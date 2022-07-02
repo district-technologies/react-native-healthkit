f2736bdb7f396328e630d4d10939a9090dceb46b
Dan <dan.ricketts@district-technologies.com>
2022-07-02 14:08:46 +1000

    feat: update example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import Healthkit, { Workout } from 'react-native-healthkit';
 
 export default function App() {
   const [status, setStatus] = React.useState<string>();
+  const [loading, setLoading] = React.useState(true);
   const [workouts, setWorkouts] = React.useState<Workout[]>();
 
-  React.useEffect(() => {
+  const refresh = React.useCallback(() => {
+    setLoading(true);
     setStatus('Checking Permissions...');
     Healthkit.requestPermissions()
       .then(async (success) => {
@@ -14,57 +23,77 @@ export default function App() {
           setStatus('Getting workouts...');
           return Healthkit.getWorkouts({
             startDate: Date.parse('2018-01-01'),
-          }).then((items) => {
-            setStatus('Workouts: ' + items.length);
-            setWorkouts(items);
-          });
+          })
+            .then((items) => {
+              setStatus('Workouts: ' + items.length);
+              setWorkouts(items);
+              setLoading(false);
+            })
+            .catch((e) => {
+              setStatus('Error getting workouts: ' + e.message);
+              setWorkouts([]);
+              setLoading(false);
+            });
         } else {
           setStatus('Permission Denied');
+          setLoading(false);
         }
       })
       .catch((e: Error) => {
         setStatus('Error:' + e.message);
+        setLoading(false);
       });
   }, []);
 
+  React.useEffect(() => {
+    refresh();
+  }, [refresh]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.statusText}>{status}</Text>
-      <ScrollView style={styles.workoutContainer}>
-        {workouts
-          ?.filter((item) => item.totalDistance > 0 && item.duration > 0)
-          ?.map((item) => {
-            return (
-              <View key={item.uuid} style={styles.workoutRow}>
-                <View style={styles.dateColumn}>
-                  <Text numberOfLines={1} ellipsizeMode="tail">
-                    {item.startDate}
-                  </Text>
-                  <Text numberOfLines={1} ellipsizeMode="tail">
-                    {item.endDate}
-                  </Text>
-                  <View style={styles.activityRow}>
-                    <Text>{item.workoutActivityType}</Text>
-                    <Text>
-                      {item.routes?.reduce((result, item) => {
-                        if (item.locations) {
-                          result += item.locations?.length;
-                        }
-                        return result;
-                      }, 0) || 'no'}{' '}
-                      locations
+      <ScrollView
+        style={styles.workoutContainer}
+        refreshControl={
+          <RefreshControl onRefresh={refresh} refreshing={loading} />
+        }
+      >
+        <Text style={styles.statusText}>{loading ? 'syncing' : status}</Text>
+        {!loading &&
+          workouts
+            ?.filter((item) => item.totalDistance > 0 && item.duration > 0)
+            ?.map((item) => {
+              return (
+                <View key={item.uuid} style={styles.workoutRow}>
+                  <View style={styles.dateColumn}>
+                    <Text numberOfLines={1} ellipsizeMode="tail">
+                      {item.startDate}
                     </Text>
-                    <Text style={styles.duration}>
-                      {(item.duration / 60).toFixed(0) + ' mins'}
+                    <Text numberOfLines={1} ellipsizeMode="tail">
+                      {item.endDate}
                     </Text>
-                    <Text style={styles.distance}>
-                      {(item.totalDistance / 1000).toFixed(2) + ' km'}
-                    </Text>
+                    <View style={styles.activityRow}>
+                      <Text>{item.workoutActivityType}</Text>
+                      <Text>
+                        {item.routes?.reduce((result, item) => {
+                          if (item.locations) {
+                            result += item.locations?.length;
+                          }
+                          return result;
+                        }, 0) || 'no'}{' '}
+                        locations
+                      </Text>
+                      <Text>{item.stepCount || 'no'} steps</Text>
+                      <Text style={styles.duration}>
+                        {(item.duration / 60).toFixed(0) + ' mins'}
+                      </Text>
+                      <Text style={styles.distance}>
+                        {(item.totalDistance / 1000).toFixed(2) + ' km'}
+                      </Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            );
-          })}
+              );
+            })}
       </ScrollView>
     </SafeAreaView>
   );
@@ -76,7 +105,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  statusText: { paddingVertical: 12 },
+  statusText: { paddingVertical: 12, textAlign: 'center' },
   workoutContainer: { width: '100%' },
   workoutRow: {
     width: '100%',
